06e54bfbb5ccc63aeac10e0491ff1fba0d3f5739
Yure Gladzik <146346870+ygladzik@users.noreply.github.com>
2026-08-08 12:40:42 -0300

    feat: configurar swagger com autenticacao jwt (#7)

---
+++ backend/src/main/java/br/com/atelieritalara/backend/config/OpenApiConfig.java
@@ -0,0 +1,15 @@
+package br.com.atelieritalara.backend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenApiConfig {
+}
